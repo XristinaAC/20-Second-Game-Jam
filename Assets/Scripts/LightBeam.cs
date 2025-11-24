2c5dbb8bc021463ad4085f8b395704f25659575e
Christina Papadodimitraki <christina-v7@hotmail.com>
2025-11-24 13:18:33 +0100

    BeamChanges

--- Assets/Scripts/LightBeam.cs
+++ Assets/Scripts/LightBeam.cs
@@ -19,22 +19,23 @@ public class LightBeam : MonoBehaviour
         //_lightBeam.startColor = Color.red;
         // _lightBeam.endColor = Color.red;
 
-        //_lightBeam.SetPosition(0, startPoint.position);
+        _lightBeam.SetPosition(0, startPoint.position);
     }
+    Ray hitRay;
+    Vector3 point;
 
     // Update is called once per frame
     void Update()
     {
-        Ray hitRay = new Ray(startPoint.position,startPoint.forward);
+        _lightBeam.SetPosition(0, startPoint.position);
+        
+        hitRay = new Ray(startPoint.position, startPoint.forward);
         RaycastHit hitInfo;
-        Debug.DrawLine(hitRay.origin, hitRay.direction * 3, Color.green);
-        if (Physics.Raycast(hitRay.origin,hitRay.direction, out hitInfo, 3,rayHitMask))
+        Debug.DrawLine(hitRay.origin, hitRay.direction * 300, Color.green);
+        if (Physics.Raycast(hitRay.origin, hitRay.direction, out hitInfo, 300, rayHitMask))
         {
-            if (hitInfo.collider.tag == "Player")
-            {
-                Debug.Log("Mirror");
-            } 
-            //_lightBeam.SetPosition(1, hitInfo.point);
+            Debug.Log("Mirror");
+            _lightBeam.SetPosition(1, hitInfo.point);
         }
     }
 }
