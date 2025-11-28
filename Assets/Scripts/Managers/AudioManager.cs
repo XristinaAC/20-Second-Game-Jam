f247ff4173fe50410b38040637cc013fab458aa0
Christina Papadodimitraki <christina-v7@hotmail.com>
2025-11-28 02:14:02 +0100

    MainMenuChanges

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,168 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance = null;
+
+    [System.Serializable]
+    public enum backgroundAudio
+    {
+        backgroundMusic,
+        levelMusic,
+        bossRoomMusic
+    }
+
+    [System.Serializable]
+    public enum effectsAudio
+    {
+        deathAudioEffect,
+        buttonAudioEffect,
+        jumpingAudioEffect,
+        glidingAudioEffect,
+        landAudioEffect,
+        uiHoverAudioEffect,
+        runningAudioEffect,
+        metalAudioEffect,
+        sizzlingPan,
+        waterEffect
+    }
+
+    [System.Serializable]
+    public struct BackgroundMusic
+    {
+        public backgroundAudio type;
+        public AudioClip clip;
+    }
+    [System.Serializable]
+    public struct SoundEffects
+    {
+        public effectsAudio type;
+        public AudioClip clip;
+    }
+
+    [Header("_____________AudioSource_______________")]
+
+    [SerializeField] private AudioSource MusicSource;
+    [SerializeField] private AudioSource SFXSource;
+    [SerializeField] private AudioMixer  audioMixer;
+
+    [SerializeField] private Slider MusicSlider;
+    [SerializeField] private Slider MasterSlider;
+    [SerializeField] private Slider SFXSlider;
+
+
+    [Header("_____________AudioClips_______________")]
+
+    [SerializeField] List<BackgroundMusic> music;
+    [SerializeField] public List<SoundEffects> effects;
+   
+    [SerializeField] public AudioClip mainMenuMusic;
+    [SerializeField] public AudioClip inGameMusic;
+    [SerializeField] public AudioClip buttonEffecct;
+    [SerializeField] public AudioClip woodButtonEffecct;
+
+    bool can_play = true;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start()
+    {
+        SetMasterVolume();
+        SetMusicVolume();
+        SetSFXVolume();
+    }
+
+    public void PlayMusic(AudioClip music)
+    {
+        MusicSource.clip = music;
+        MusicSource.Play();
+    }
+
+    //public void PlayMusic(backgroundAudio musicT)
+    //{
+    //    for (int i = 0; i < music.Count; i++)
+    //    {
+    //        if (musicT == music[i].type)
+    //        {
+    //            MusicSource.clip = music[i].clip;
+    //            MusicSource.Play();
+    //        }
+    //    }
+    //}
+
+    public void PlaySFX(AudioClip sfxEffect)
+    {
+        if(SFXSource.isPlaying)
+        {
+            can_play = false;
+            //SFXSource.Stop();
+            StartCoroutine(Reset());
+        }
+
+        //SFXSource.clip = sfxEffect;
+        SFXSource.PlayOneShot(sfxEffect);
+
+        //StartCoroutine(Reset());
+    }
+
+    IEnumerator Reset()
+    {
+        yield return new WaitForSeconds(0.2f);
+        can_play = true;
+    }
+
+    //public void PlaySFX(effectsAudio effectT)
+    //{
+    //    for (int i = 0; i < effects.Count; i++)
+    //    {
+    //        if (effectT == effects[i].type)
+    //        {
+    //            SFXSource.clip = effects[i].clip;
+    //            SFXSource.Play();
+    //        }
+    //    }
+    //}
+
+    //public void StopPlayingMusic(backgroundAudio musicT)
+    //{
+    //    for (int i = 0; i < music.Count; i++)
+    //    {
+    //        if (musicT == music[i].type)
+    //        {
+    //            MusicSource.clip = music[i].clip;
+    //            MusicSource.Stop();
+    //        }
+    //    }
+    //}
+
+    public void SetMasterVolume()
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(MasterSlider.value) * 20);
+    }
+
+    public void SetMusicVolume()
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(MusicSlider.value) * 20);
+    }
+
+    public void SetSFXVolume()
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(SFXSlider.value) * 20);
+    }
+}
--- Assets/Scripts/Managers/SettingsMenuManager.cs
+++ Assets/Scripts/Managers/SettingsMenuManager.cs
@@ -1,11 +1,32 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenuManager : MonoBehaviour
 {
+    [Serializable]
+    struct WindowModes
+    {
+        public string mode;
+        public bool fullscreen;
+    }
     public static SettingsMenuManager Instance = null;
 
     [SerializeField] GameObject audioMenu;
     [SerializeField] GameObject graphicsMenu;
+    [SerializeField] Toggle VSync = null;
+    [SerializeField] List<Vector2> resolutions = new List<Vector2>();
+    //[SerializeField] List<string> window_modes = new List<string>();
+    [SerializeField] List<WindowModes> window_modes = new List<WindowModes>();
+    [SerializeField] TMP_Text resolutions_text = null;
+    [SerializeField] TMP_Text window_mode_text = null;
+    
+    int selected_resolution = 0;
+    int selected_wm = 0;
+    public Vector2 resolution = new();
+    bool fullscreen;
 
     private void Awake()
     {
@@ -23,13 +44,22 @@ public class SettingsMenuManager : MonoBehaviour
 
     private void Start()
     {
+        Screen.fullScreen = true;
+        window_mode_text.text = window_modes[0].mode;
         this.gameObject.SetActive(false);
-        //graphicsMenu.SetActive(true);
+        if (QualitySettings.vSyncCount == 0)
+        {
+            VSync.isOn = false;
+        }
+        else
+        {
+            VSync.isOn = true;
+        }
     }
 
     void Update()
     {
-        
+
     }
 
     public void AudioMenu()
@@ -43,4 +73,108 @@ public class SettingsMenuManager : MonoBehaviour
         graphicsMenu.SetActive(true);
         audioMenu.SetActive(false);
     }
+
+    public void Close_Settings()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void Res_Left_Arrow()
+    {
+        selected_resolution--;
+        if (selected_resolution < 0)
+        {
+            selected_resolution = 0;
+        }
+
+        Update_Resolutions_Text();
+    }
+
+    public void Res_Right_Arrow()
+    {
+        selected_resolution++;
+        if (selected_resolution > resolutions.Count - 1)
+        {
+            selected_resolution = resolutions.Count - 1;
+        }
+
+        Update_Resolutions_Text();
+    }
+
+    public void Update_Resolutions_Text()
+    {
+        resolutions_text.text = resolutions[selected_resolution].x.ToString() + " x " + resolutions[selected_resolution].y.ToString();
+    }
+
+    public void Apply_Changes()
+    {
+        if (VSync.isOn)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+        else
+        {
+            QualitySettings.vSyncCount = 0;
+        }
+
+        Screen.SetResolution((int)resolutions[selected_resolution].x, (int)resolutions[selected_resolution].y, fullscreen);
+    }
+
+
+    public void wm_Res_Left_Arrow()
+    {
+        selected_wm--;
+        if (selected_wm < 0)
+        {
+            selected_wm = 0;
+        }
+
+        Update_WindowMode_Text();
+    }
+
+    public void wm_Res_Right_Arrow()
+    {
+        selected_wm++;
+        if (selected_wm > window_modes.Count - 1)
+        {
+            selected_wm = window_modes.Count - 1;
+        }
+
+        Update_WindowMode_Text();
+    }
+
+    public void Update_WindowMode_Text()
+    {
+        window_mode_text.text = window_modes[selected_wm].mode;
+        fullscreen = window_modes[selected_wm].fullscreen;
+    }
 }
+
+//public void Start()
+//{
+//    this.gameObject.SetActive(false);
+//    fullscreen.isOn = Screen.fullScreen;
+//    //sound_volume.value = audio.volume;
+
+//    if (QualitySettings.vSyncCount == 0)
+//    {
+//        VSync.isOn = false;
+//    }
+//    else
+//    {
+//        VSync.isOn = true;
+//    }
+//}
+
+
+//public void Close_Settings()
+//{
+//    AudioManager.Instance.PlaySFX(AudioManager.Instance.buttonEffecct);
+//    if (this.gameObject.activeInHierarchy)
+//    {
+//        this.gameObject.SetActive(false);
+//    }
+//}
+
+
+
